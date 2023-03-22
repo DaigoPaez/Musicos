726f96e813a3a400a49bf10e27e557912c76cac8
DaigoPaez <126517336+DaigoPaez@users.noreply.github.com>
2023-03-22 19:57:55 +0000

    Por ahí va

---
+++ Musicos/Program.cs
@@ -0,0 +1,50 @@
+﻿class Musico
+{
+    public string Nombre {get;set;}
+    public Musico (string n){Nombre=n;}
+    public void Saluda(){Console.WriteLine($"Hola soy {Nombre}");}
+    public virtual void Toca()
+    {
+        Console.WriteLine($"{Nombre} tocando su instrumento");
+    }
+}
+
+class Bajista:Musico
+{
+    public string bajo {get;set;}
+    public Bajista(string n, string b):base(n)
+    {
+        bajo=b;
+    }
+    public new void Toca()
+    {
+            Console.WriteLine($"{Nombre} tocando su {bajo}");
+    }
+}
+
+class Baterista:Musico
+{
+    public string bateria {get;set;}
+    public Baterista(string n, string b):base(n)
+    {
+        bateria=b;
+    }
+        public new void Toca()
+    {
+            Console.WriteLine($"{Nombre} tocando su {bateria}");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+    List<Musico> grupo=new List<Musico>();
+    grupo.Add(new Baterista("Mario", "batería"));
+    grupo.Add(new Bajista("Joe", "bajo"));
+
+    foreach(var m in grupo)
+        m.Saluda();
+    foreach(var m in grupo)
+        m.Toca();
+    }
+}
